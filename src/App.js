465a0456deceea810192abaae99bc0c4f065f7de
christina-yun <poncheeyun@yahoo.com>
2021-08-13 11:13:04 -0700

    Added some formatting -- figuring out opacity

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [listOfCharacters, setListOfCharacters] = useState([]);
-  const [character, setCharacter] = useState(null);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -32,10 +31,11 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      {listOfCharacters.map(object => {
-        return <Character key={object.index} character={object} />
-      })}
-      
+        <div className='allcards'>
+          {listOfCharacters.map((object, index) => {
+            return <Character key={index} character={object} />
+          })}
+        </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -5,7 +5,29 @@ import styled from 'styled-components';
 //styling the components
 const StyledCharacter = styled.div`
     display:flex;
-    margin: 0 5rem;
+    margin: 0 10rem;
+
+    .card{
+        display:flex;
+        margin:1rem;
+
+    }
+    .name{
+        width:20rem;
+        background-color:blue;
+        opacity:1;
+    }
+    h2{
+        margin:
+    }
+
+    .other-info{
+        margin:0 2rem;
+        background-color:pink;
+        opacity:1.5;
+    }
+
+    p{}
 `
 
 function Character(props){
@@ -13,14 +35,17 @@ function Character(props){
 
     return (
         <StyledCharacter>
-            <h2>{character.name}</h2>
-            {/* <p>Homeworld: {character.homeworld}</p> */}
-            <div className='other-info'>
-            <p>Birth Year: {character['birth_year']}</p>
-            <p>Hair Color: {character['hair_color']}</p>
-            <p>Eye Color: {character['eye_color']}</p>    
+            <div className='card'>
+                <div className='name'>
+                   <h2>{character.name}</h2>
+                </div>
+                <div className='other-info'>
+                    {/* <p>Homeworld: {character.homeworld}</p> */}
+                    <p>Birth Year: {character['birth_year']}</p>
+                    <p>Hair Color: {character['hair_color']}</p>
+                    <p>Eye Color: {character['eye_color']}</p>    
+                </div>
             </div>
-
         </StyledCharacter>
     )
 }
